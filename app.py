3123e2c5f67849a8617f7a1c6e40aa521b935e40
Lurker_Ubuntu <rdarshan2005@gmail.com>
2026-02-26 20:17:53 +0530

    after chanign it to bervo

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = flask.Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
+app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
